693244a8a26a2565ba2efa9ec64a06d609c69035
Mario Cecchi <macecchi@gmail.com>
2014-02-06 15:05:09 +0000

    Initial Commit

---
+++ Shopping List/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Shopping List
+//
+//  Created by Mario Cecchi on 2/6/14.
+//  Copyright (c) 2014 Mario Cecchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
